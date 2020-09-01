070b2790ad0563b00755f01868396b38c49ad549
Matheus <rsantos.matheus@outlook.com>
2020-09-01 10:51:37 -0300

    Chess layer and print board

--- src/boardGame/Position.java
+++ src/boardGame/Position.java
@@ -1,28 +1,28 @@
 package boardGame;
 
 public class Position {
-	private int rows;
-	private int columns;
+	private int row;
+	private int column;
 	
-	public Position(int rows, int columns) {
-		this.rows = rows;
-		this.columns = columns;
+	public Position(int row, int column) {
+		this.row = row;
+		this.column = column;
 	}
-	public int getRows() {
-		return rows;
+	public int getRow() {
+		return row;
 	}
-	public void setRows(int rows) {
-		this.rows = rows;
+	public void setRow(int row) {
+		this.row = row;
 	}
-	public int getColumns() {
-		return columns;
+	public int getColumn() {
+		return column;
 	}
-	public void setColumns(int columns) {
-		this.columns = columns;
+	public void setColumn(int column) {
+		this.column = column;
 	}
 	@Override
 	public String toString() {
-		return "Linha:" + rows + ", coluna:" + columns;
+		return "Linha:" + row + ", coluna:" + column;
 	}
 	
 	
