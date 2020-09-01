531086f7dd737562e88999bbf24d79272acdba94
Matheus <rsantos.matheus@outlook.com>
2020-09-01 10:17:24 -0300

    Começo da implementação das peças

--- src/boardGame/Board.java
+++ src/boardGame/Board.java
@@ -1,30 +1,31 @@
 package boardGame;
 
-public class Position {
+public class Board {
 	private int rows;
 	private int columns;
+	private Piece[][] pieces;
+		
+	public Board(int rows, int columns) {
 	
-	public Position(int rows, int columns) {
 		this.rows = rows;
 		this.columns = columns;
+		pieces = new Piece[rows][columns];
 	}
+
 	public int getRows() {
 		return rows;
 	}
+
 	public void setRows(int rows) {
 		this.rows = rows;
 	}
+
 	public int getColumns() {
 		return columns;
 	}
+
 	public void setColumns(int columns) {
 		this.columns = columns;
 	}
-	@Override
-	public String toString() {
-		return "Linha:" + rows + ", coluna:" + columns;
-	}
-	
-	
 
 }
