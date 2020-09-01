cbfe29ed736af8c5566621038556cd86871c2b41
Matheus <rsantos.matheus@outlook.com>
2020-09-01 11:00:10 -0300

    Tabuleiro e camada de xadrez

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,16 @@
 package application;
 
 import boardGame.Board;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Board board = new Board (8,8);
-	System.out.println(board);
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
+		
+		System.out.println();
 	
 	}
 
